25bca02e17dbfeea0d0266e6da388e203e3f2dee
iodel <iodeliakos@gmail.com>
2019-05-29 03:39:18 +0300

    Set up Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgMaterialModule } from './ng-material/ng-material.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    NgMaterialModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
