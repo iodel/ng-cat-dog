bd6b2bf4c4eab52a82132fd7abb08eacc9614bdf
iodel <iodeliakos@gmail.com>
2019-06-18 00:38:19 +0300

    Import Material modules

--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
-  exports: [MatButtonModule, MatCheckboxModule]
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ]
 })
 export class NgMaterialModule { }
